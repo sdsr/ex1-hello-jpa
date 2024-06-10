88562b2c1d9506a5de59f401363842b21b3303be
Taewook <dlxodnr1212@gmail.com>
2024-06-10 18:02:51 +0900

    Query, paging

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -15,16 +17,25 @@ public class JpaMain {
         //code
         try {
             Member member = new Member();
-            member.setId(1L);
-            member.setName("helloA");
+//            member.setId(1L);
+//            member.setName("helloA");
 
 //            member.setId(2L);
 //            member.setName("helloB");
 
-            em.persist(member);
-            Member findMember = em.find(Member.class, 1L);
+//            em.persist(member);
+//            Member findMember = em.find(Member.class, 1L);
 //            em.remove(member);//삭제
-            findMember.setName("HelloJPA");//수정
+//            findMember.setName("HelloJPA");//수정
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member1 : result) {
+                System.out.println("member1.getName() = " + member1.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
